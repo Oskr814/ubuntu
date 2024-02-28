0a7a1568f85e242223017e2bda35b75f1894f997
oskr814 <oskralvarez814@gmail.com>
2024-02-28 09:34:19 -0600

    refactor: zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,18 @@
-# Auto suggestions plugin
+echo "Configuring Zsh"
+
+# Cleaing old instalations
+apt remove zsh
+rm -rf /home/$USER/.zshrc
+rm -rf /home/$USER/.oh-my-zsh
+
+apt install zsh -y
+zsh --version
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Custom plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i '$a\plugins=(git ubuntu docker zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
 
 # Theme
 mkdir ~/.fonts
@@ -11,8 +22,8 @@ wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/Meslo
 wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i '$a\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
-source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
+sed -i 's/^plugins=.*/plugins=(git ubuntu docker zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
 source ~/.zshrc
 
 p10k configure

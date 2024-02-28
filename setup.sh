1e1bc026326e107013b9d30a6651d0071456103d
oskr814 <oskralvarez814@gmail.com>
2024-02-28 09:33:32 -0600

    refactor: remove sudo from commands

--- setup.sh
+++ setup.sh
@@ -9,19 +9,19 @@ if [ "$install_docker" == "y" ]; then
     echo "Installing Docker"
     # Docker installation
     # Add Docker's official GPG key:
-    sudo apt-get install ca-certificates curl -y
-    sudo install -m 0755 -d /etc/apt/keyrings -y
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    apt-get install ca-certificates curl -y
+    install -m 0755 -d /etc/apt/keyrings -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
+      tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
 
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 else
     echo "Skipping Docker installation."
 fi
@@ -53,11 +53,6 @@ fi
 # Configure Zsh
 read -p "Do you want to configure Zsh? (y/n): " configure_zsh
 if [ "$configure_zsh" == "y" ]; then
-    echo "Configuring Zsh"
-    apt install zsh -y
-    zsh --version
-    chsh -s $(which zsh)
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     bash ./zsh.sh
 else
     echo "Skipping Zsh configuration."
